62cf664a4f0af3e4992d766fdb974547c34e00c7
Isaiah Norton <inorton@bwh.harvard.edu>
2013-10-17 17:20:33 -0400

    Set PYTHONHOME based on current interpreter

--- src/PyCall.jl
+++ src/PyCall.jl
@@ -334,7 +334,13 @@ function dlopen_libpython(python::String)
     libpaths = [pyconfigvar(python, "LIBDIR"),
                 (@windows ? dirname(pysys(python, "executable")) : joinpath(dirname(dirname(pysys(python, "executable"))), "lib"))]
     @osx_only push!(libpaths, pyconfigvar(python, "PYTHONFRAMEWORKPREFIX"))
-    @windows_only push!(libpaths, pyconfigvar(python, "exec_prefix"))
+    @windows_only begin
+        exec_prefix = pyconfigvar(python, "exec_prefix")
+        push!(libpaths, exec_prefix)
+        if !haskey(ENV, "PYTHONHOME")
+            ENV["PYTHONHOME"] = exec_prefix
+        end
+    end
     # TODO: look in python-config output? pyconfigvar("LDFLAGS")?
     for libpath in libpaths
         if isfile(joinpath(libpath, lib))
