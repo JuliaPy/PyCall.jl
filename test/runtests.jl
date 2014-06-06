4d2bc2f840cb73abf5c56882c8d6998c7b5eb0ec
Steven G. Johnson <stevenj@mit.edu>
2014-06-06 14:30:59 -0400

    slight test tweak

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,7 +63,7 @@ array2py2arrayeq(x) = PyCall.py2array(Float64,PyCall.array2py(x)) == x
 @pyimport math
 @test_approx_eq math.sin(3) sin(3)
 
-@test collect(PyObject([1,3,5])) == [1,3,5]
+@test collect(PyObject([1,"hello",5])) == [1,"hello",5]
 
 @test try @eval (@pyimport os.path) catch ex isa(ex, ArgumentError) end
 
