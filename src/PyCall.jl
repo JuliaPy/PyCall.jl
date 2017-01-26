d3aaecb8bf6d60a811b4849eb91ddf6a14d8b272
Jon Malmaud <malmaud@gmail.com>
2017-01-26 15:22:52 -0500

    Add tests

--- src/PyCall.jl
+++ src/PyCall.jl
@@ -708,7 +708,7 @@ for (mime, method) in ((MIME"text/html", "_repr_html_"),
 end
 
 #########################################################################
-# Expose Python docstrings to Julia doc system
+# Expose Python docstrings to the Julia doc system
 
 if isdefined(Docs, :getdoc)  # Introduced in Julia .6
     Docs.getdoc(o::PyObject) = Text(String(o["__doc__"]))
