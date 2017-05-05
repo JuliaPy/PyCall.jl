fbcef2983c5118756f86959bb182dafbc4295c65
Eric Davies <iamed2@gmail.com>
2017-05-05 14:00:52 -0500

    Address review comments on style and type introspection

--- src/serialize.jl
+++ src/serialize.jl
@@ -1,29 +1,28 @@
 const _pickle = PyNULL()
 
-pickle() = _pickle.o == C_NULL ? copy!(_pickle, pyimport("pickle")) : _pickle
+function pickle()
+    if _pickle.o == C_NULL
+        if pyexists(:cPickle)
+            copy!(_pickle, pyimport(:cPickle))
+        else
+            copy!(_pickle, pyimport(:pickle))
+        end
+    end
+
+    return _pickle
+end
 
 function Base.serialize(s::AbstractSerializer, pyo::PyObject)
     Base.serialize_type(s, PyObject)
-
-    b = PyBuffer(pycall(pickle()[:dumps], PyObject, pyo))
-
+    b = PyBuffer(pycall(pickle()["dumps"], PyObject, pyo))
     serialize(s, unsafe_wrap(Array, Ptr{UInt8}(pointer(b)), sizeof(b)))
 end
 
 function Base.deserialize(s::AbstractSerializer, t::Type{PyObject})
     b = deserialize(s)
 
-    pycall(
-        pickle()[:loads],
-        PyObject,
-        PyObject(
-            @pycheckn ccall(
-                @pysym(PyString_FromStringAndSize),
-                PyPtr,
-                (Ptr{UInt8}, Int),
-                b,
-                sizeof(b)
-            )
-        )
-    )
+    pycall(pickle()["loads"], PyObject,
+        PyObject(@pycheckn ccall(@pysym(PyString_FromStringAndSize),
+                PyPtr, (Ptr{UInt8}, Int),
+                b, sizeof(b))))
 end
