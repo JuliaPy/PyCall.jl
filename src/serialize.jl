9aa06c1877d06ca4cd9a864e4735607682ba5f7b
Eric Davies <iamed2@gmail.com>
2017-05-08 11:55:38 -0500

    Better handle serialization/deserialization of NULL PyObjects

--- src/serialize.jl
+++ src/serialize.jl
@@ -3,16 +3,24 @@ const _pickle = PyNULL()
 pickle() = _pickle.o == C_NULL ? copy!(_pickle, pyimport(PyCall.pyversion.major ≥ 3 ? "pickle" : "cPickle")) : _pickle
 
 function Base.serialize(s::AbstractSerializer, pyo::PyObject)
-    pyo.o == C_NULL && return invoke(serialize, (AbstractSerializer, Any), s, pyo)
     Base.serialize_type(s, PyObject)
-    b = PyBuffer(pycall(pickle()["dumps"], PyObject, pyo))
-    serialize(s, unsafe_wrap(Array, Ptr{UInt8}(pointer(b)), sizeof(b)))
+    if pyo.o == C_NULL
+        serialize(s, pyo.o)
+    else
+        b = PyBuffer(pycall(pickle()["dumps"], PyObject, pyo))
+        serialize(s, unsafe_wrap(Array, Ptr{UInt8}(pointer(b)), sizeof(b)))
+    end
 end
 
 function Base.deserialize(s::AbstractSerializer, t::Type{PyObject})
     b = deserialize(s)
-    return isa(b, PyPtr) ? PyObject(b) : pycall(pickle()["loads"], PyObject,
-        PyObject(@pycheckn ccall(@pysym(PyString_FromStringAndSize),
-                PyPtr, (Ptr{UInt8}, Int),
-                b, sizeof(b))))
+    if isa(b, PyPtr)
+        @assert b == C_NULL
+        return PyNULL()
+    else
+        return pycall(pickle()["loads"], PyObject,
+            PyObject(@pycheckn ccall(@pysym(PyString_FromStringAndSize),
+                    PyPtr, (Ptr{UInt8}, Int),
+                    b, sizeof(b))))
+    end
 end
