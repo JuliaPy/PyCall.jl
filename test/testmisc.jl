eec331dbcca0da72cc73d995db9778d73e662a0f
Joel Mason <jobba1@hotmail.com>
2018-04-07 03:08:37 +1000

    numpy.array to array.array

--- test/testmisc.jl
+++ test/testmisc.jl
@@ -1,16 +1,15 @@
 using Compat.Test, PyCall
 
-PyCall.@npyinitialize # see #481
-
 @testset "pair call syntax" begin
-    np = pyimport("numpy")
-    arrpyo = np["array"]=>PyObject
-    arrpytpl = np["array"]=>Tuple{Vararg{Int}}
-    arrpyvec = np["array"]=>Vector{Int}
-    arrpyany = np["array"]=>PyAny
-    @test typeof(arrpyo(1:10)) == PyObject
-    @test arrpytpl(1:10) isa Tuple{Vararg{Int}}
-    @test typeof(arrpytpl(1:10)) == NTuple{10, Int}
-    @test typeof(arrpyvec(1:10)) == Vector{Int}
-    @test typeof(arrpyany(1:10)) == Vector{Int}
+    pyarr = pyimport("array")["array"]
+    arrpyo = pyarr=>PyObject
+    arrpytpl = pyarr=>Tuple{Vararg{Float64}}
+    arrpyvec = pyarr=>Vector{Float64}
+    arrpyany = pyarr=>PyAny
+    @test typeof(arrpyo("d", 1.0:10.0)) == PyObject
+    @test arrpytpl("d", 1.0:10.0) isa Tuple{Vararg{Float64}}
+    @test typeof(arrpytpl("d", 1.0:10.0)) == NTuple{10, Float64}
+    @test typeof(arrpyvec("d", 1.0:10.0)) == Vector{Float64}
+    # broken with __array_interface__ but not with buffer protocol
+    @test_broken typeof(arrpyany("d", 1.0:10.0)) == Vector{Float64}
 end
