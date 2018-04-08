17f5329c8ef647b510ee5e4c05a77e3f81ede983
Joel Mason <jobba1@hotmail.com>
2018-04-09 01:31:37 +1000

    apparently array.array doesn't support the buffer protocol in py 2.7

--- test/testmisc.jl
+++ test/testmisc.jl
@@ -1,15 +1,14 @@
 using Compat.Test, PyCall
 
 @testset "pair call syntax" begin
-    pyarr = pyimport("array")["array"]
-    arrpyo = pyarr=>PyObject
-    arrpytpl = pyarr=>Tuple{Vararg{Float64}}
-    arrpyvec = pyarr=>Vector{Float64}
-    arrpyany = pyarr=>PyAny
-    @test typeof(arrpyo("d", 1.0:10.0)) == PyObject
-    @test arrpytpl("d", 1.0:10.0) isa Tuple{Vararg{Float64}}
-    @test typeof(arrpytpl("d", 1.0:10.0)) == NTuple{10, Float64}
-    @test typeof(arrpyvec("d", 1.0:10.0)) == Vector{Float64}
-    # broken with __array_interface__ but not with buffer protocol
-    @test_broken typeof(arrpyany("d", 1.0:10.0)) == Vector{Float64}
+    pylist = pybuiltin("list")
+    listpyo = pylist=>PyObject
+    listpytpl = pylist=>Tuple{Vararg{Float64}}
+    listpyvec = pylist=>Vector{Float64}
+    listpyany = pylist=>PyAny
+    @test typeof(listpyo(1.0:10.0)) == PyObject
+    @test listpytpl(1.0:10.0) isa Tuple{Vararg{Float64}}
+    @test typeof(listpytpl(1.0:10.0)) == NTuple{10, Float64}
+    @test typeof(listpyvec(1.0:10.0)) == Vector{Float64}
+    @test typeof(listpyany(1.0:10.0)) == Vector{Float64}
 end
