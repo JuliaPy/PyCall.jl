1826f4ad7025a0a7b7249e3969f3b16fd0d6940c
Joel Mason <jobba1@hotmail.com>
2018-04-11 22:26:05 +1000

    Add pydecref to old ret value, and fix bug in benchmark

--- benchmarks/callperf.jl
+++ benchmarks/callperf.jl
@@ -6,40 +6,30 @@ let
     np = pyimport("numpy")
     nprand = np["random"]["rand"]
     nprand_pyo(sz...) = pycall(nprand, PyObject, sz...)
-    nprand2d_wrap = PyFuncWrap(nprand, (Int, Int))
+    ret = PyNULL()
+    args_lens = (0,3,7,12,17)
+    arr_sizes = (ntuple(i->1, len) for len in args_lens)
+    nprand_wraps = [PyFuncWrap(nprand, map(typeof, arr_size)) for arr_size in arr_sizes]
+    @show typeof(nprand_wraps)
+    for (i, arr_size) in enumerate(arr_sizes)
+        nprand_wrap = nprand_wraps[i]
+        arr_size_str = args_lens[i] < 5 ? "$arr_size" : "$(args_lens[i])*(1,1,...)"
+        results["nprand_pyo $arr_size_str"] = @benchmark $nprand_pyo($arr_size...)
+        println("nprand_pyo $arr_size_str:\n"); display(results["nprand_pyo $arr_size_str"])
+        println("--------------------------------------------------")
 
-    arr_size = (2,2)
+        results["nprand_wrap $arr_size_str"] = @benchmark $nprand_wrap($arr_size...)
+        println("nprand_wrap $arr_size_str:\n"); display(results["nprand_wrap $arr_size_str"])
+        println("--------------------------------------------------")
 
-    results["nprand_pyo"] = @benchmark $nprand_pyo($arr_size...)
-    println("nprand_pyo:\n"); display(results["nprand_pyo"])
-    println("--------------------------------------------------")
-
-    results["nprand2d_wrap"] = @benchmark $nprand2d_wrap($arr_size...)
-    println("nprand2d_wrap:\n"); display(results["nprand2d_wrap"])
-    println("--------------------------------------------------")
-
-    # args already set by nprand2d_wrap calls above
-    results["nprand2d_wrap_noargs"] = @benchmark $nprand2d_wrap()
-    println("nprand2d_wrap_noargs:\n"); display(results["nprand2d_wrap_noargs"])
-    println("--------------------------------------------------")
-
-    arr_size = ntuple(i->2, 15)
-
-    results["nprand_pyo2"] = @benchmark $nprand_pyo($arr_size...)
-    println("nprand_pyo2:\n"); display(results["nprand_pyo2"])
-    println("--------------------------------------------------")
-
-    results["nprand2d_wrap2"] = @benchmark $nprand2d_wrap($arr_size...)
-    println("nprand2d_wrap2:\n"); display(results["nprand2d_wrap2"])
-    println("--------------------------------------------------")
-
-    # args already set by nprand2d_wrap calls above
-    results["nprand2d_wrap_noargs2"] = @benchmark $nprand2d_wrap()
-    println("nprand2d_wrap_noargs2:\n"); display(results["nprand2d_wrap_noargs2"])
-    println("--------------------------------------------------")
+        # args already set by nprand_wrap calls above
+        results["nprand_wrap_noargs $arr_size_str"] = @benchmark $nprand_wrap()
+        println("nprand_wrap_noargs $arr_size_str:\n"); display(results["nprand_wrap_noargs $arr_size_str"])
+        println("--------------------------------------------------")
+    end
 end
 
 println("")
 println("Mean times")
 println("----------")
-foreach((r)->println(rpad(r[1],23), ": ", mean(r[2])), results)
+foreach((r)->println(rpad(r[1],33), ": ", mean(r[2])), results)
