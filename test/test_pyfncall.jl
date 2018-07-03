ee4d6f78f3e545768939744c7cc09657a5e19e2e
Joel Mason <jobba1@hotmail.com>
2018-07-04 00:50:07 +0300

    Add tests for kwargs

--- test/test_pyfncall.jl
+++ test/test_pyfncall.jl
@@ -1,26 +1,37 @@
-using Compat.Test, PyCall
+using Compat, Compat.Test, PyCall
 
 py"""
-def mklist(*args):
-    return list(args)
+def mklist(*args, **kwargs):
+    l = list(args)
+    l.extend(kwargs.items())
+    return l
 """
 @testset "pycall!" begin
     pymklist = py"mklist"
-    res = PyNULL()
+    ret = PyNULL()
 
-    function pycall_checks(res, pyf, RetType, args...)
-        pycall_res = pycall(pyf, RetType, args...)
-        res_converted = pycall!(res, pyf, RetType, args...)
-        @test convert(RetType, res) == res_converted
+    function pycall_checks(res, pyf, RetType, args...; kwargs...)
+        pycall_res = pycall(pyf, RetType, args...; kwargs...)
+        res_converted = pycall!(res, pyf, RetType, args...; kwargs...)
         @test res_converted == pycall_res
+        @test convert(RetType, res) == res_converted
         RetType != PyAny && @test res_converted isa RetType
     end
 
     @testset "basics" begin
         args = ("a", 2, 4.5)
         for RetType in (PyObject, PyAny, Tuple)
-            pycall_checks(res, pymklist, RetType, args...)
+            pycall_checks(ret, pymklist, RetType, args...)
             GC.gc()
         end
     end
+
+    @testset "kwargs" begin
+        args = ("a", 2, 4.5)
+        for RetType in (PyObject, PyAny, Tuple)
+            pycall_checks(ret, pymklist, RetType, args...; b=19610, c="5")
+            GC.gc()
+        end
+    end
+
 end
