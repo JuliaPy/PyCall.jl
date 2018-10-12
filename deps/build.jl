1ba2b8d4d535fbab73807b86588ee4529fc2a53f
Steven G. Johnson <stevenj@mit.edu>
2018-10-12 16:08:03 -0400

    store prefs in depot_path/prefs (#589)
    * store prefs in depot_path/prefs
    
    As a stopgap until we get proper package options (JuliaLang/Juleps#38), store PYTHON preference in depot_path/prefs/PyCall so that they aren't forgotten when PyCall is updated
    
    * whoops

--- deps/build.jl
+++ deps/build.jl
@@ -141,9 +141,12 @@ function whichfirst(args...)
     return ""
 end
 
+const prefsfile = VERSION < v"0.7" ? "PYTHON" : joinpath(first(DEPOT_PATH), "prefs", "PyCall")
+mkpath(dirname(prefsfile))
+
 try # make sure deps.jl file is removed on error
     python = try
-        let py = get(ENV, "PYTHON", isfile("PYTHON") ? readchomp("PYTHON") :
+        let py = get(ENV, "PYTHON", isfile(prefsfile) ? readchomp(prefsfile) :
                      (Compat.Sys.isunix() && !Compat.Sys.isapple()) || Sys.ARCH ∉ (:i686, :x86_64) ?
                      whichfirst("python3", "python") : "Conda"),
             vers = isempty(py) || py == "Conda" ? v"0.0" : vparse(pyconfigvar(py,"VERSION","0.0"))
@@ -227,7 +230,7 @@ try # make sure deps.jl file is removed on error
     """)
 
     # Make subsequent builds (e.g. Pkg.update) use the same Python by default:
-    writeifchanged("PYTHON", use_conda ? "Conda" : isfile(programname) ? programname : python)
+    writeifchanged(prefsfile, use_conda ? "Conda" : isfile(programname) ? programname : python)
 
     #########################################################################
 
