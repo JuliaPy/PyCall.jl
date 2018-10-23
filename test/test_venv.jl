7866c931b05864442f585f374b055a141e7b0dcd
Takafumi Arakaki <aka.tkf@gmail.com>
2018-10-22 21:21:11 -0700

    Test with virtualenv command as well

--- test/test_venv.jl
+++ test/test_venv.jl
@@ -27,6 +27,84 @@ using Compat: @info, @warn
 end
 
 
+function test_venv_has_python(path)
+    newpython = PyCall.python_cmd(venv=path).exec[1]
+    if !isfile(newpython)
+        @info """
+        Python executable $newpython does not exists.
+        This directory contains only the following files:
+        $(join(readdir(dirname(newpython)), '\n'))
+        """
+    end
+    @test isfile(newpython)
+end
+
+
+function test_venv_activation(path)
+    newpython = PyCall.python_cmd(venv=path).exec[1]
+
+    # Run a fresh Julia process with new Python environment
+    if VERSION < v"0.7.0-"
+        setup_code = ""
+    else
+        setup_code = Base.load_path_setup_code()
+    end
+    code = """
+    $setup_code
+    using PyCall
+    println(PyCall.pyimport("sys")[:executable])
+    println(PyCall.pyimport("sys")[:exec_prefix])
+    println(PyCall.pyimport("pip")[:__file__])
+    """
+    # Note that `pip` is just some arbitrary non-standard
+    # library.  Using standard library like `os` does not work
+    # because those files are not created.
+    env = copy(ENV)
+    env["PYCALL_JL_RUNTIME_PYTHON"] = newpython
+    jlcmd = setenv(`$(Base.julia_cmd()) --startup-file=no -e $code`, env)
+    if Compat.Sys.iswindows()
+        # Marking the test broken in Windows.  It seems that
+        # venv copies .dll on Windows and libpython check in
+        # PyCall.__init__ detects that.
+        @test_broken begin
+            output = read(jlcmd, String)
+            sys_executable, exec_prefix, mod_file = split(output, "\n")
+            newpython == sys_executable
+        end
+    else
+        output = read(jlcmd, String)
+        sys_executable, exec_prefix, mod_file = split(output, "\n")
+        @test newpython == sys_executable
+        @test startswith(exec_prefix, path)
+        @test startswith(mod_file, path)
+    end
+end
+
+
+@testset "virtualenv activation" begin
+    if Compat.Sys.which("virtualenv") === nothing
+        @info "No virtualenv command. Skipping the test..."
+    else
+        mktempdir() do path
+            run(`virtualenv $path`)
+            test_venv_has_python(path)
+
+            newpython = PyCall.python_cmd(venv=path).exec[1]
+            venv_libpython = PyCall.find_libpython(newpython)
+            if venv_libpython != PyCall.libpython
+                @info """
+                virtualenv created an environment with incompatible libpython:
+                    $venv_libpython
+                """
+                return
+            end
+
+            test_venv_activation(path)
+        end
+    end
+end
+
+
 @testset "venv activation" begin
     if Compat.Sys.isunix() && !startswith(PyCall.pyprogramname, "/usr/bin")
         @warn """
@@ -51,51 +129,8 @@ end
         mktempdir() do path
             # Create a new virtual environment
             run(PyCall.python_cmd(`-m venv $path`))
-            newpython = PyCall.python_cmd(venv=path).exec[1]
-            if !isfile(newpython)
-                @info """
-                Python executable $newpython does not exists.
-                This directory contains only the following files:
-                $(join(readdir(dirname(newpython)), '\n'))
-                """
-            end
-            @test isfile(newpython)
-
-            # Run a fresh Julia process with new Python environment
-            if VERSION < v"0.7.0-"
-                setup_code = ""
-            else
-                setup_code = Base.load_path_setup_code()
-            end
-            code = """
-            $setup_code
-            using PyCall
-            println(PyCall.pyimport("sys")[:executable])
-            println(PyCall.pyimport("sys")[:exec_prefix])
-            println(PyCall.pyimport("pip")[:__file__])
-            """
-            # Note that `pip` is just some arbitrary non-standard
-            # library.  Using standard library like `os` does not work
-            # because those files are not created.
-            env = copy(ENV)
-            env["PYCALL_JL_RUNTIME_PYTHON"] = newpython
-            jlcmd = setenv(`$(Base.julia_cmd()) --startup-file=no -e $code`, env)
-            if Compat.Sys.iswindows()
-                # Marking the test broken in Windows.  It seems that
-                # venv copies .dll on Windows and libpython check in
-                # PyCall.__init__ detects that.
-                @test_broken begin
-                    output = read(jlcmd, String)
-                    sys_executable, exec_prefix, mod_file = split(output, "\n")
-                    newpython == sys_executable
-                end
-            else
-                output = read(jlcmd, String)
-                sys_executable, exec_prefix, mod_file = split(output, "\n")
-                @test newpython == sys_executable
-                @test startswith(exec_prefix, path)
-                @test startswith(mod_file, path)
-            end
+            test_venv_has_python(path)
+            test_venv_activation(path)
         end
     end
 end
