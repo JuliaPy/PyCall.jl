637651ad18733c8bf57b85a4ad1529ba59656747
Takafumi Arakaki <aka.tkf@gmail.com>
2019-02-24 22:42:19 -0800

    Add Pkg.activate in aot/precompile.jl

--- aot/precompile.jl
+++ aot/precompile.jl
@@ -1,3 +1,10 @@
+# Activate ./Project.toml.  Excluding `"@v#.#"` from `Base.LOAD_PATH`
+# to make compilation more reproducible.
+using Pkg
+empty!(Base.LOAD_PATH)
+append!(Base.LOAD_PATH, ["@", "@stdlib"])
+Pkg.activate(@__DIR__)
+
 # Manually invoking `__init__` to workaround:
 # https://github.com/JuliaLang/julia/issues/22910
 
