96e1843426622b48f088684d841ba36575b3eccc
Takafumi Arakaki <aka.tkf@gmail.com>
2020-04-24 20:54:08 -0700

    Fix AOT tests for PackageCompiler 1.x

--- aot/compile.jl
+++ aot/compile.jl
@@ -4,22 +4,22 @@
 exec "${JULIA:-julia}" "$@" ${BASH_SOURCE[0]}
 =#
 
+using Libdl
+using PackageCompiler
 using Pkg
+
 Pkg.activate(@__DIR__)
-Pkg.add("MacroTools")
 Pkg.develop(PackageSpec(name="PyCall", path=dirname(@__DIR__)))
 Pkg.build("PyCall")
 Pkg.activate()
 
-using PackageCompiler
-sysout, _curr_syso = compile_incremental(
-    joinpath(@__DIR__, "Project.toml"),
-    joinpath(@__DIR__, "precompile.jl"),
+sysimage_path = joinpath(@__DIR__, "sys.$(Libdl.dlext)")
+create_sysimage(
+    sysimage_path = sysimage_path,
+    project = @__DIR__(),
+    precompile_execution_file = joinpath(@__DIR__, "precompile.jl"),
 )
-
-pysysout = joinpath(@__DIR__, basename(sysout))
-cp(sysout, pysysout, force=true)
 
 write(joinpath(@__DIR__, "_julia_path"), Base.julia_cmd().exec[1])
 
-@info "System image: $pysysout"
+@info "System image: $sysimage_path"
