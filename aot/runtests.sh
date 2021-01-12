09ab06ae5c56725bb990431693ba76c58d47c001
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-01-12 04:37:02 -0500

    Add --project when running tests

--- aot/runtests.sh
+++ aot/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 thisdir="$(dirname "${BASH_SOURCE[0]}")"
-exec "$thisdir/julia.sh" --startup-file=no --color=yes -e '
+exec "$thisdir/julia.sh" --startup-file=no --color=yes -e --project $thisdir '
 using Pkg
 Pkg.test("PyCall")
 '
