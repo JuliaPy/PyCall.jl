f05803c7aa66eefb6935a582325b40044f12e28f
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-01-12 04:50:03 -0500

    Fix runtests.sh

--- aot/runtests.sh
+++ aot/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 thisdir="$(dirname "${BASH_SOURCE[0]}")"
-exec "$thisdir/julia.sh" --startup-file=no --color=yes --project $thisdir -e '
+exec "$thisdir/julia.sh" --startup-file=no --color=yes --project=$thisdir -e '
 using Pkg
 Pkg.test("PyCall")
 '
