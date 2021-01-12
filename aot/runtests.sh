42eda17fd87e7ec41bd92e40b5db618218d8019d
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-01-12 04:41:54 -0500

    Fix runtests.sh arg order

--- aot/runtests.sh
+++ aot/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 thisdir="$(dirname "${BASH_SOURCE[0]}")"
-exec "$thisdir/julia.sh" --startup-file=no --color=yes -e --project $thisdir '
+exec "$thisdir/julia.sh" --startup-file=no --color=yes --project $thisdir -e '
 using Pkg
 Pkg.test("PyCall")
 '
