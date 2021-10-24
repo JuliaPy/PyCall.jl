77d8ee089dd19d9fe0bcbcc5a954cb712f8edfec
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-23 21:12:24 -0400

    Fix check_deps_version.jl

--- test/check_deps_version.jl
+++ test/check_deps_version.jl
@@ -1,9 +1,16 @@
 using Test
-const desired_version = VersionNumber(ARGS[1])
+const desired_version_str = ARGS[1]  # e.g., "2.7", "3.10.0", "3.x"
 include("../deps/deps.jl")
-@testset "pyversion_build ≈ $desired_version" begin
+@testset "pyversion_build ≈ $desired_version_str" begin
+    v = desired_version_str
+    while endswith(v, ".x")
+        v = v[1:end-2]
+    end
+    desired_version = VersionNumber(v)
     @test desired_version.major == pyversion_build.major
-    @test desired_version.minor == pyversion_build.minor
+    if desired_version.minor != 0
+        @test desired_version.minor == pyversion_build.minor
+    end
     if desired_version.patch != 0
         @test desired_version.patch == pyversion_build.patch
     end
